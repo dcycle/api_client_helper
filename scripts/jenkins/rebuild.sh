a8111c27e604e708801c901f62088c4fd35114ee
alberto56 <albert@mediatribe.net>
2020-11-26 10:32:22 -0500

    Jenkins rebuild script

--- scripts/jenkins/rebuild.sh
+++ scripts/jenkins/rebuild.sh
@@ -1,1 +1,21 @@
-docker build -t dcycle/api_client_helper:1 .
+#!/bin/bash
+set -e
+source ~/.docker-host-ssh-credentials
+
+# Create a droplet
+PRIVATE_IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
+  "./digitalocean/scripts/new-droplet.sh docker-api-helper")
+# https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
+IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
+echo "Created VM at $IP"
+sleep 90
+
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  root@"$IP" "mkdir -p docker-api-helper-job"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  ~/.dcycle-docker-credentials.sh \
+  root@$IP:~/.dcycle-docker-credentials.sh
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  -r * root@"$IP":docker-api-helper-job
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  root@"$IP" "cd docker-api-helper-job && ls -lah && ./scripts/rebuild.sh"
