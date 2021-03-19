80fdb428b18ed97c0f02a6bf19cb9e6206f14765
Albert Albala <albert@mediatribe.net>
2021-03-19 11:11:57 -0400

    Do not assume that python is installed, only Docker, during testing

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -21,13 +21,10 @@ echo "=> Running all test_*.py files"
 echo "=>"
 ./scripts/run-all-test-files.sh
 echo "=>"
-echo "=> Testing with dummy dummy"
+echo "=> Testing with dummy dummy, do not pretty print with"
+echo "=> '| python -m json.tool' because python may not always be installed"
 echo "=>"
 docker run --rm local-dcycle-api-client-helper-image dummy dummy
-echo "=>"
-echo "=> Testing with dummy dummy, pretty print"
-echo "=>"
-docker run --rm local-dcycle-api-client-helper-image dummy dummy | python -m json.tool
 echo "=>"
 echo "=> Testing with dummy multistep"
 echo "=>"
